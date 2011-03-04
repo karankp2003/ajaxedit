6c5fb1d6618e04d9087fe39861561d39968fe86a
karankp2003 <karankp2003@gmail.com>
2011-03-03 19:08:35 -0500

    added nifty auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110118011039) do
+ActiveRecord::Schema.define(:version => 20110303234902) do
 
   create_table "oceans", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
